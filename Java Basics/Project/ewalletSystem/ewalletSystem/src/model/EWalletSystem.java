d43b49b183a04d30881fff51a7c1f0bc33323380
Mohamedayman5674-sudo <medoayman5674@gmail.com>
2026-01-27 18:12:30 +0200

    Update EWalletSystem.java

--- Java Basics/Project/ewalletSystem/ewalletSystem/src/model/EWalletSystem.java
+++ Java Basics/Project/ewalletSystem/ewalletSystem/src/model/EWalletSystem.java
@@ -8,15 +8,14 @@ public class EWalletSystem {
     private final String name = "Eraa Soft E Wallet System";
     private List<Account> accounts = new ArrayList<>();
 
-    // ===== Constructor =====
+
     public EWalletSystem() {
         createDefaultAdmin();
     }
 
-    // ===== Auto Admin =====
+ 
     private void createDefaultAdmin() {
 
-        // نتأكد إن الأدمن مش موجود
         boolean adminExists = accounts.stream()
                 .anyMatch(acc -> "IAM".equals(acc.getUsername()));
 
@@ -32,13 +31,12 @@ public class EWalletSystem {
             );
             admin.setAdmin(true);
             admin.setActive(true);
-            admin.addTransaction("Admin account created automatically");
 
             accounts.add(admin);
         }
     }
 
-    // ===== Getters =====
+
     public String getName() {
         return name;
     }
